226f6b0a519aaa02b7ea1c0ab40bfb840271474e
Brooke Scott <42704084+brookescott87@users.noreply.github.com>
2024-03-29 13:09:25 -0400

    Added local gguf install directory to module search path

--- convert.py
+++ convert.py
@@ -29,6 +29,7 @@ from sentencepiece import SentencePieceProcessor
 
 if 'NO_LOCAL_GGUF' not in os.environ:
     sys.path.insert(1, str(Path(__file__).parent / 'gguf-py'))
+    sys.path.insert(2, str(Path(__file__).parent.parent / 'lib' / 'python'))
 import gguf
 
 if TYPE_CHECKING:
