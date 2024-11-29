5769d5240c4d790b841b191404efab447768bd78
Brooke <brooketh@quantasia>
2024-11-28 22:16:21 -0500

    Allow overriding the class of np.memmap

--- gguf-py/gguf/gguf_reader.py
+++ gguf-py/gguf/gguf_reader.py
@@ -86,8 +86,8 @@ class GGUFReader:
         GGUFValueType.BOOL:    np.bool_,
     }
 
-    def __init__(self, path: os.PathLike[str] | str, mode: Literal['r', 'r+', 'c'] = 'r'):
-        self.data = np.memmap(path, mode = mode)
+    def __init__(self, path: os.PathLike[str] | str, mode: Literal['r', 'r+', 'c'] = 'r', cls: type = np.memmap):
+        self.data = cls(path, mode = mode)
         offs = 0
 
         # Check for GGUF magic
